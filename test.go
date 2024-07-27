20a4fdd95524cf475a30aab38c6765907fe0396c
athena <gyqonly@163.com>
2024-07-27 18:05:30 +0800

    test

--- test.go
+++ test.go
@@ -3,13 +3,18 @@ package main
 import "fmt"
 
 func main() {
-
 	test1()
+	test2()
 }
 
 func test1() {
 	s := []string{"a", "b", "c"}
 	for i, v := range s {
+		fmt.Println("bbbb")
 		fmt.Printf("idx:%d, val:%s\n", i, v)
 	}	
+}
+
+func test2() {
+	fmt.Println("good job")
 }
